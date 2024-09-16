4bc3704ac4e3a202a53b39f8db39dc6658a220de
iAmKevinKhyle <gepulle.vin@gmail.com>
2024-09-16 16:42:04 +0800

    Added Config

--- controllers/novel-controllers.js
+++ controllers/novel-controllers.js
@@ -3,10 +3,20 @@ import * as cheerio from "cheerio";
 
 const url = "https://novelbin.me/";
 
+const agents = [
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+  "Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0",
+];
+
 const config = {
   headers: {
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "user-agent": agents[Math.floor(Math.random() * agents.length)],
+    "upgrade-insecure-requests": "1",
+    accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9,en;q=0.8",
   },
 };
 
